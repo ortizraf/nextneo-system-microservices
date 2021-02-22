86a5609eb51ec84b25af8e2cd32a09d8d8260a0e
Rafael Ortiz <rafaelortiz@MRXMBP11.local>
2021-02-21 21:08:53 -0300

    Improvements

--- currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.nextneo.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -13,8 +16,14 @@ public class CurrencyConversionController {
 	//http://localhost:8100//currency-conversion/from/USD/to/BRL/quantity/10
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		
-		CurrencyConversion currencyConversion = new CurrencyConversion(10001l, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE);		
-		return currencyConversion;
+		HashMap<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		return new CurrencyConversion(10001l, from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()));		
 	}
 
 }
