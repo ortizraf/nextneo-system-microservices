2aed456a0091433c9ecd15477eb3e0364e8d25e4
Rafael Ortiz <rafaelortiz@MRXMBP11.local>
2021-02-28 00:33:33 -0300

    Add eureka server

--- currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.nextneo.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	CurrencyExchangeProxy proxy;
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	//http://localhost:8100//currency-conversion/from/USD/to/BRL/quantity/10
@@ -22,6 +26,15 @@ public class CurrencyConversionController {
 		
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		return new CurrencyConversion(10001l, from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()));		
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	//http://localhost:8100//currency-conversion/from/USD/to/BRL/quantity/10
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversion(10001l, from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()));		
 	}
