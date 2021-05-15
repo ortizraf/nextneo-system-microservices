e8764f13fa7a238453a25e837286222c406f9d70
Rafael Ortiz <rafaelortiz@MRXMBP11.local>
2021-05-15 17:12:39 -0300

    Config

--- currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "nextneo-currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "nextneo-currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "nextneo-currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
