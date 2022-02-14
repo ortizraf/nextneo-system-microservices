56486cd6497bfab00a7c5c753e32b3d5a9062668
Rafael Ortiz <ortizrafael1988@gmail.com>
2022-02-13 21:30:51 -0300

    Add configmap to application

--- currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/nextneo/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 //@FeignClient(name = "nextneo-currency-exchange-service", url = "localhost:8000")
 //@FeignClient(name = "nextneo-currency-exchange-service")
 public interface CurrencyExchangeProxy {
